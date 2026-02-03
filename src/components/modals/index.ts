38e174097b57939c2abbd24921a583e74f657919
Ikundabayо Placide <ikundabayoplacide500@gmail.com>
2026-02-03 15:10:19 +0200

    modified codes for customer dashboard

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,3 +1,3 @@
 export { default as AddUserModal } from './AddUserModal'
 export { default as AddRoleModal } from './AddRoleModal'
-export { default as DeleteCustomer } from './DeleteCustomer'
+// export { default as DeleteCustomer } from './DeleteCustomer'
